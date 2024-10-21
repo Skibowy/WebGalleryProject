907760049fb54fea3c4b41843ecb069a2c8640c6
PassivelyIronic <150748075+PassivelyIronic@users.noreply.github.com>
2024-10-21 19:57:08 +0200

    UserImagesVM

---
+++ Models/UserImagesVIewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebGalleryProject.Models
+{
+    public class UserImagesViewModel
+    {
+        public string UserName { get; set; } // Nazwa użytkownika
+        public List<Image> Images { get; set; } // Lista obrazów
+    }
+}
